b69bd1d1871e2fe71731ba2d7f98e5b580dd4574
Leah Hirschfeld <leah.hirschfeld@gmail.com>
2020-09-10 19:29:53 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,17 @@
-#write your code here
+def countdown(number)
+  until number == 0
+      puts "#{number} SECOND(S)!"
+      number -= 1 
+    end
+  if number == 0
+    return "HAPPY NEW YEAR!"
+  end
+end
 
-def countdown
+def countdown_with_sleep(number)
+  until number == 0 
+    puts "#{number} SECONDS!"
+      sleep 1  
+      number -= 1 
+    end
 end
